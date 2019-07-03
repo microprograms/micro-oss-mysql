353b109bc407332aba7f86152d9a6771f71ef2c6
xuzewei <microprograms@126.com>
2019-07-03 14:25:12 +0800

    v1.2.4

--- src/main/java/com/github/microprograms/micro_oss_mysql/model/ddl/TableColumnDefinition.java
+++ src/main/java/com/github/microprograms/micro_oss_mysql/model/ddl/TableColumnDefinition.java
@@ -3,34 +3,36 @@ package com.github.microprograms.micro_oss_mysql.model.ddl;
 import com.github.microprograms.micro_oss_core.model.FieldDefinition.FieldTypeEnum;
 
 public class TableColumnDefinition implements TableElementDefinition {
-    private String name;
-    private String type;
+	private String name;
+	private String type;
 
-    public TableColumnDefinition(String name, String type) {
-        this.name = name;
-        this.type = type;
-    }
+	public TableColumnDefinition(String name, String type) {
+		this.name = name;
+		this.type = type;
+	}
 
-    public static String getIgniteType(FieldTypeEnum fieldType, boolean isPrimaryKey) {
-        switch (fieldType) {
-        case int_type:
-            return "int";
-        case long_type:
-            return "bigint";
-        case string_type:
-            return isPrimaryKey ? "varchar(99)" : "text";
-        default:
-            return null;
-        }
-    }
+	public static String getMysqlDataType(FieldTypeEnum fieldType, boolean isPrimaryKey) {
+		switch (fieldType) {
+		case int_type:
+			return "int";
+		case long_type:
+			return "bigint";
+		case string_type:
+			return isPrimaryKey ? "varchar(99)" : "text";
+		case datetime_type:
+			return "datetime";
+		default:
+			return null;
+		}
+	}
 
-    @Override
-    public String toText() {
-        return "`" + name + "` " + type;
-    }
+	@Override
+	public String toText() {
+		return "`" + name + "` " + type;
+	}
 
-    @Override
-    public String toString() {
-        return toText();
-    }
+	@Override
+	public String toString() {
+		return toText();
+	}
 }
