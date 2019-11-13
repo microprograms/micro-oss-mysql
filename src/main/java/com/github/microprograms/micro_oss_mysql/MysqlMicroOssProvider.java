8a029f4e656c4f830b9f692c3d71f59cff28704f
xuzewei <microprograms@126.com>
2019-11-13 13:25:44 +0800

    v3.2.0

--- src/main/java/com/github/microprograms/micro_oss_mysql/MysqlMicroOssProvider.java
+++ src/main/java/com/github/microprograms/micro_oss_mysql/MysqlMicroOssProvider.java
@@ -1,76 +1,64 @@
 package com.github.microprograms.micro_oss_mysql;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.sql.DataSource;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.github.microprograms.micro_oss_core.MicroOssProvider;
+import com.github.microprograms.micro_oss_core.QueryResult;
 import com.github.microprograms.micro_oss_core.Transaction;
 import com.github.microprograms.micro_oss_core.exception.MicroOssException;
 import com.github.microprograms.micro_oss_core.model.Entity;
-import com.github.microprograms.micro_oss_core.model.FieldDefinition;
-import com.github.microprograms.micro_oss_core.model.TableDefinition;
+import com.github.microprograms.micro_oss_core.model.Field;
 import com.github.microprograms.micro_oss_core.model.ddl.CreateTableCommand;
 import com.github.microprograms.micro_oss_core.model.ddl.DropTableCommand;
-import com.github.microprograms.micro_oss_core.model.dml.DeleteCommand;
-import com.github.microprograms.micro_oss_core.model.dml.InsertCommand;
-import com.github.microprograms.micro_oss_core.model.dml.SelectCommand;
-import com.github.microprograms.micro_oss_core.model.dml.SelectCountCommand;
-import com.github.microprograms.micro_oss_core.model.dml.UpdateCommand;
-import com.github.microprograms.micro_oss_mysql.model.ddl.PrimaryKeyDefinition;
-import com.github.microprograms.micro_oss_mysql.model.ddl.TableColumnDefinition;
-import com.github.microprograms.micro_oss_mysql.model.ddl.TableElementDefinition;
+import com.github.microprograms.micro_oss_core.model.dml.query.Condition;
+import com.github.microprograms.micro_oss_core.model.dml.query.PagerRequest;
+import com.github.microprograms.micro_oss_core.model.dml.query.SelectCommand;
+import com.github.microprograms.micro_oss_core.model.dml.query.SelectCountCommand;
+import com.github.microprograms.micro_oss_core.model.dml.query.Sort;
+import com.github.microprograms.micro_oss_core.model.dml.update.DeleteCommand;
+import com.github.microprograms.micro_oss_core.model.dml.update.InsertCommand;
+import com.github.microprograms.micro_oss_core.model.dml.update.UpdateCommand;
 import com.github.microprograms.micro_oss_mysql.utils.MysqlUtils;
 
 public class MysqlMicroOssProvider implements MicroOssProvider {
 	private static final Logger log = LoggerFactory.getLogger(MysqlMicroOssProvider.class);
-	private Config config;
 
-	public MysqlMicroOssProvider(Config config) {
-		this.config = config;
-		try {
-			Class.forName(config.getDriver());
-		} catch (ClassNotFoundException e) {
-			log.error("canot load jdbc driver", e);
-		}
+	private DataSource dataSource;
+
+	public MysqlMicroOssProvider(DataSource dataSource) {
+		this.dataSource = dataSource;
 	}
 
-	public Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(config.getUrl(), config.getUser(), config.getPassword());
+	protected String getTableName(Class<?> clz) {
+		return clz.getSimpleName();
+	}
+
+	protected Entity buildEntity(Object javaObject) {
+		JSONObject json = (JSONObject) JSON.toJSON(javaObject);
+		List<Field> fields = new ArrayList<>();
+		for (String key : json.keySet()) {
+			fields.add(new Field(key, json.get(key)));
+		}
+		return new Entity(getTableName(javaObject.getClass()), fields);
 	}
 
 	@Override
 	public void createTable(CreateTableCommand command) throws MicroOssException {
-		try (Connection conn = getConnection()) {
-			StringBuffer sb = new StringBuffer("CREATE TABLE IF NOT EXISTS ");
-			TableDefinition tableDefinition = command.getTableDefinition();
-			sb.append(tableDefinition.getTableName());
-			sb.append("(");
-			PrimaryKeyDefinition primaryKeyDefinition = new PrimaryKeyDefinition();
-			List<TableElementDefinition> tableElementDefinitions = new ArrayList<>();
-			for (FieldDefinition fieldDefinition : tableDefinition.getFields()) {
-				boolean isPrimaryKey = fieldDefinition.getPrimaryKey() > 0;
-				if (isPrimaryKey) {
-					primaryKeyDefinition.getFiledNames().put(fieldDefinition.getPrimaryKey(),
-							fieldDefinition.getName());
-				}
-				tableElementDefinitions
-						.add(new TableColumnDefinition(fieldDefinition.getName(), fieldDefinition.getComment(),
-								TableColumnDefinition.getMysqlDataType(fieldDefinition.getType(), isPrimaryKey)));
-			}
-			tableElementDefinitions.add(primaryKeyDefinition);
-			sb.append(StringUtils.join(tableElementDefinitions, ","));
-			String tableComment = StringUtils.isBlank(tableDefinition.getComment()) ? ""
-					: tableDefinition.getComment().replaceAll("'", "''");
-			sb.append(String.format(") COMMENT='%s';", tableComment));
-			String sql = sb.toString();
-			log.debug("executeUpdate> {}", sql);
+		try (Connection conn = dataSource.getConnection()) {
+			String sql = MysqlUtils.buildSql(command);
+			log.debug("createTable> {}", sql);
 			conn.createStatement().executeUpdate(sql);
 		} catch (Exception e) {
 			throw new MicroOssException(e);
@@ -79,9 +67,9 @@ public class MysqlMicroOssProvider implements MicroOssProvider {
 
 	@Override
 	public void dropTable(DropTableCommand command) throws MicroOssException {
-		try (Connection conn = getConnection()) {
+		try (Connection conn = dataSource.getConnection()) {
 			String sql = String.format("DROP TABLE IF EXISTS %s;", command.getTableName());
-			log.debug("executeUpdate> {}", sql);
+			log.debug("dropTable> {}", sql);
 			conn.createStatement().executeUpdate(sql);
 		} catch (Exception e) {
 			throw new MicroOssException(e);
@@ -89,9 +77,9 @@ public class MysqlMicroOssProvider implements MicroOssProvider {
 	}
 
 	@Override
-	public long deleteObject(DeleteCommand command) throws MicroOssException {
-		try (Connection conn = getConnection()) {
-			String sql = MysqlUtils.buildSql(command);
+	public int insertObject(Object object) throws MicroOssException {
+		try (Connection conn = dataSource.getConnection()) {
+			String sql = MysqlUtils.buildSql(new InsertCommand(buildEntity(object)));
 			log.debug("executeUpdate> {}", sql);
 			return conn.createStatement().executeUpdate(sql);
 		} catch (Exception e) {
@@ -100,9 +88,9 @@ public class MysqlMicroOssProvider implements MicroOssProvider {
 	}
 
 	@Override
-	public long insertObject(InsertCommand command) throws MicroOssException {
-		try (Connection conn = getConnection()) {
-			String sql = MysqlUtils.buildSql(command);
+	public int updateObject(Class<?> clz, List<Field> fields, Condition where) throws MicroOssException {
+		try (Connection conn = dataSource.getConnection()) {
+			String sql = MysqlUtils.buildSql(new UpdateCommand(getTableName(clz), fields, where));
 			log.debug("executeUpdate> {}", sql);
 			return conn.createStatement().executeUpdate(sql);
 		} catch (Exception e) {
@@ -111,56 +99,88 @@ public class MysqlMicroOssProvider implements MicroOssProvider {
 	}
 
 	@Override
-	public List<Entity> selectObject(SelectCommand command) throws MicroOssException {
-		try (Connection conn = getConnection()) {
-			String sql = MysqlUtils.buildSql(command);
-			log.debug("executeQuery> {}", sql);
-			return MysqlUtils.getEntityList(command.getTableName(), conn.createStatement().executeQuery(sql));
+	public int deleteObject(Class<?> clz, Condition where) throws MicroOssException {
+		try (Connection conn = dataSource.getConnection()) {
+			String sql = MysqlUtils.buildSql(new DeleteCommand(getTableName(clz), where));
+			log.debug("executeUpdate> {}", sql);
+			return conn.createStatement().executeUpdate(sql);
 		} catch (Exception e) {
 			throw new MicroOssException(e);
 		}
 	}
 
 	@Override
-	public long selectCount(SelectCountCommand command) throws MicroOssException {
-		try (Connection conn = getConnection()) {
+	public int queryCount(Class<?> clz, Condition where) throws MicroOssException {
+		try (Connection conn = dataSource.getConnection()) {
+			SelectCountCommand command = new SelectCountCommand(getTableName(clz), where);
 			StringBuffer sb = new StringBuffer("SELECT COUNT(*) AS count FROM ").append(command.getTableName());
-			String where = MysqlUtils.parse(command.getWhere());
-			if (StringUtils.isNotBlank(where)) {
-				sb.append(" WHERE ").append(where);
+			String whereCondition = MysqlUtils.parse(command.getWhere());
+			if (StringUtils.isNotBlank(whereCondition)) {
+				sb.append(" WHERE ").append(whereCondition).append(";");
 			}
 			String sql = sb.toString();
 			log.debug("executeQuery> {}", sql);
-			return MysqlUtils.getCount(conn.createStatement().executeQuery(sql));
+			ResultSet rs = conn.createStatement().executeQuery(sql);
+			rs.next();
+			return rs.getObject("count", Integer.class);
 		} catch (Exception e) {
 			throw new MicroOssException(e);
 		}
 	}
 
 	@Override
-	public long updateObject(UpdateCommand command) throws MicroOssException {
-		try (Connection conn = getConnection()) {
+	public <T> QueryResult<T> query(Class<T> clz, Condition where) throws MicroOssException {
+		return query(clz, null, where, null, null);
+	}
+
+	@Override
+	public <T> QueryResult<T> query(Class<T> clz, Condition where, List<Sort> sorts) throws MicroOssException {
+		return query(clz, null, where, sorts, null);
+	}
+
+	@Override
+	public <T> QueryResult<T> query(Class<T> clz, Condition where, List<Sort> sorts, PagerRequest pager)
+			throws MicroOssException {
+		return query(clz, null, where, sorts, pager);
+	}
+
+	@Override
+	public <T> QueryResult<T> query(Class<T> clz, List<String> fieldNames, Condition where, List<Sort> sorts,
+			PagerRequest pager) throws MicroOssException {
+		try (Connection conn = dataSource.getConnection()) {
+			SelectCommand command = new SelectCommand(getTableName(clz), fieldNames, where, sorts, pager);
 			String sql = MysqlUtils.buildSql(command);
-			log.debug("executeUpdate> {}", sql);
-			return conn.createStatement().executeUpdate(sql);
+			log.debug("executeQuery> {}", sql);
+			List<Entity> entities = MysqlUtils.getEntityList(command.getTableName(),
+					conn.createStatement().executeQuery(sql));
+			return new QueryResult<>(entities, clz);
 		} catch (Exception e) {
 			throw new MicroOssException(e);
 		}
 	}
 
 	@Override
-	public Transaction beginTransaction() throws MicroOssException {
+	public void execute(Transaction transaction) throws MicroOssException {
+		Connection conn = null;
 		try {
-			Connection connection = getConnection();
-			connection.setAutoCommit(false);
-			return new MysqlTransaction(connection);
+			conn = dataSource.getConnection();
+			conn.setAutoCommit(false);
+			transaction.execute(this);
+			conn.commit();
 		} catch (Exception e) {
+			try {
+				conn.rollback();
+			} catch (SQLException sqlException) {
+				// ignore
+			}
 			throw new MicroOssException(e);
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException sqlException) {
+				// ignore
+			}
 		}
 	}
 
-	@Override
-	public void close() throws MicroOssException {
-		log.debug("close");
-	}
 }
--- src/main/java/com/github/microprograms/micro_oss_mysql/utils/MysqlUtils.java
+++ src/main/java/com/github/microprograms/micro_oss_mysql/utils/MysqlUtils.java
@@ -1,8 +1,5 @@
 package com.github.microprograms.micro_oss_mysql.utils;
 
-import java.io.Closeable;
-import java.io.IOException;
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -13,203 +10,193 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.github.microprograms.micro_oss_core.model.Entity;
 import com.github.microprograms.micro_oss_core.model.Field;
-import com.github.microprograms.micro_oss_core.model.dml.ComplexCondition;
-import com.github.microprograms.micro_oss_core.model.dml.Condition;
-import com.github.microprograms.micro_oss_core.model.dml.DeleteCommand;
-import com.github.microprograms.micro_oss_core.model.dml.InsertCommand;
-import com.github.microprograms.micro_oss_core.model.dml.PagerRequest;
-import com.github.microprograms.micro_oss_core.model.dml.SelectCommand;
-import com.github.microprograms.micro_oss_core.model.dml.Sort;
-import com.github.microprograms.micro_oss_core.model.dml.UpdateCommand;
+import com.github.microprograms.micro_oss_core.model.FieldDefinition;
+import com.github.microprograms.micro_oss_core.model.TableDefinition;
+import com.github.microprograms.micro_oss_core.model.ddl.CreateTableCommand;
+import com.github.microprograms.micro_oss_core.model.dml.query.Condition;
+import com.github.microprograms.micro_oss_core.model.dml.query.Condition.ComplexCondition;
+import com.github.microprograms.micro_oss_core.model.dml.query.PagerRequest;
+import com.github.microprograms.micro_oss_core.model.dml.query.SelectCommand;
+import com.github.microprograms.micro_oss_core.model.dml.query.Sort;
+import com.github.microprograms.micro_oss_core.model.dml.update.DeleteCommand;
+import com.github.microprograms.micro_oss_core.model.dml.update.InsertCommand;
+import com.github.microprograms.micro_oss_core.model.dml.update.UpdateCommand;
+import com.github.microprograms.micro_oss_mysql.model.ddl.PrimaryKeyDefinition;
+import com.github.microprograms.micro_oss_mysql.model.ddl.TableColumnDefinition;
+import com.github.microprograms.micro_oss_mysql.model.ddl.TableElementDefinition;
 
 public class MysqlUtils {
 
-    public static String parse(Condition where) {
-        if (where == null) {
-            return null;
-        } else if (where instanceof ComplexCondition) {
-            ComplexCondition complex = (ComplexCondition) where;
-            if (complex.getConditions().isEmpty()) {
-                return null;
-            }
-            List<String> childStringList = new ArrayList<>();
-            for (Condition child : complex.getConditions()) {
-                if (child == null) {
-                    continue;
-                }
-                String childString = parse(child);
-                if (StringUtils.isBlank(childString)) {
-                    continue;
-                }
-                if (child instanceof ComplexCondition) {
-                    childStringList.add("(" + childString + ")");
-                } else {
-                    childStringList.add(childString);
-                }
-            }
-            return StringUtils.join(childStringList, getSeparator(complex.getType()));
-        } else {
-            return String.format("%s %s", where.getKey(), getSqlValue(where.getValue()));
-        }
-    }
+	public static String buildSql(CreateTableCommand command) {
+		StringBuffer sb = new StringBuffer("CREATE TABLE IF NOT EXISTS ");
+		TableDefinition tableDefinition = command.getTableDefinition();
+		sb.append(tableDefinition.getTableName());
+		sb.append("(");
+		PrimaryKeyDefinition primaryKeyDefinition = new PrimaryKeyDefinition();
+		List<TableElementDefinition> tableElementDefinitions = new ArrayList<>();
+		for (FieldDefinition fieldDefinition : tableDefinition.getFields()) {
+			boolean isPrimaryKey = fieldDefinition.getPrimaryKey() > 0;
+			if (isPrimaryKey) {
+				primaryKeyDefinition.getFiledNames().put(fieldDefinition.getPrimaryKey(), fieldDefinition.getName());
+			}
+			tableElementDefinitions
+					.add(new TableColumnDefinition(fieldDefinition.getName(), fieldDefinition.getComment(),
+							TableColumnDefinition.getMysqlDataType(fieldDefinition.getType(), isPrimaryKey)));
+		}
+		tableElementDefinitions.add(primaryKeyDefinition);
+		sb.append(StringUtils.join(tableElementDefinitions, ","));
+		String tableComment = StringUtils.isBlank(tableDefinition.getComment()) ? ""
+				: tableDefinition.getComment().replaceAll("'", "''");
+		sb.append(String.format(") COMMENT='%s';", tableComment));
+		return sb.toString();
+	}
 
-    private static String getSeparator(ComplexCondition.TypeEnum type) {
-        switch (type) {
-        case and:
-            return " and ";
-        case or:
-            return " or ";
-        default:
-            throw new RuntimeException("Unsupported Type");
-        }
-    }
+	public static String buildSql(InsertCommand command) {
+		List<String> fieldNames = new ArrayList<>();
+		List<String> fieldValues = new ArrayList<>();
+		for (Field field : command.getEntity().getFields()) {
+			fieldNames.add(getSqlField(field.getName()));
+			String value = getSqlValue(field.getValue());
+			fieldValues.add(value == null ? "null" : value);
+		}
+		return String.format("INSERT INTO %s (%s) VALUES (%s);", command.getEntity().getTableName(),
+				StringUtils.join(fieldNames, ","), StringUtils.join(fieldValues, ","));
+	}
 
-    private static String getSqlField(String field) {
-        return "`" + field + "`";
-    }
+	public static String buildSql(UpdateCommand command) {
+		StringBuffer sb = new StringBuffer("UPDATE ").append(command.getTableName());
+		List<Field> fields = command.getFields();
+		if (null == fields || fields.isEmpty()) {
+			return null;
+		}
+		List<String> pairs = new ArrayList<>();
+		for (Field x : fields) {
+			pairs.add(String.format("%s=%s", getSqlField(x.getName()), getSqlValue(x.getValue())));
+		}
+		sb.append(" SET ").append(StringUtils.join(pairs, ","));
+		String where = MysqlUtils.parse(command.getWhere());
+		if (StringUtils.isNotBlank(where)) {
+			sb.append(" WHERE ").append(where);
+		}
+		return sb.append(";").toString();
+	}
 
-    private static String getSqlValue(Object value) {
-        if (value instanceof String) {
-            return "'" + value.toString().replaceAll("'", "''") + "'";
-        }
-        if (value instanceof Integer) {
-            return value.toString();
-        }
-        if (value instanceof Long) {
-            return value.toString();
-        }
-        return null;
-    }
+	public static String buildSql(DeleteCommand command) {
+		StringBuffer sb = new StringBuffer("DELETE FROM ").append(command.getTableName());
+		String where = MysqlUtils.parse(command.getWhere());
+		if (StringUtils.isNoneBlank(where)) {
+			sb.append(" WHERE ").append(where);
+		}
+		return sb.append(";").toString();
+	}
 
-    public static String parseSorts(List<Sort> sorts) {
-        if (sorts == null || sorts.isEmpty()) {
-            return null;
-        }
-        List<String> list = new ArrayList<>();
-        for (Sort x : sorts) {
-            list.add(String.format("%s %s", getSqlField(x.getFieldName()), x.getType()));
-        }
-        return StringUtils.join(list, ",");
-    }
+	public static String buildSql(SelectCommand command) {
+		StringBuffer sb = new StringBuffer("SELECT ");
+		sb.append(command.getFieldNames() == null || command.getFieldNames().isEmpty() ? "*"
+				: StringUtils.join(command.getFieldNames(), ","));
+		sb.append(" FROM ").append(command.getTableName());
+		String where = MysqlUtils.parse(command.getWhere());
+		if (StringUtils.isNotBlank(where)) {
+			sb.append(" WHERE ").append(where);
+		}
+		String sort = MysqlUtils.parseSorts(command.getSorts());
+		if (StringUtils.isNotBlank(sort)) {
+			sb.append(" ORDER BY ").append(sort);
+		}
+		String pager = MysqlUtils.parse(command.getPager());
+		if (StringUtils.isNotBlank(pager)) {
+			sb.append(" ").append(pager);
+		}
+		return sb.append(";").toString();
+	}
 
-    public static String parse(PagerRequest pagerRequest) {
-        if (pagerRequest == null) {
-            return null;
-        }
-        return String.format("LIMIT %s OFFSET %s", pagerRequest.getPageSize(), pagerRequest.getPageIndex() * pagerRequest.getPageSize());
-    }
+	public static String parse(Condition where) {
+		if (null == where) {
+			return null;
+		} else if (where instanceof ComplexCondition) {
+			ComplexCondition complex = (ComplexCondition) where;
+			Condition[] conditions = complex.getConditions();
+			if (null == conditions || conditions.length == 0) {
+				return null;
+			}
+			List<String> childStringList = new ArrayList<>();
+			for (Condition child : complex.getConditions()) {
+				if (null == child) {
+					continue;
+				}
+				String childString = parse(child);
+				if (StringUtils.isBlank(childString)) {
+					continue;
+				}
+				if (child instanceof ComplexCondition) {
+					childStringList.add("(" + childString + ")");
+				} else {
+					childStringList.add(childString);
+				}
+			}
+			return StringUtils.join(childStringList, getSeparator(complex.getType()));
+		} else {
+			return String.format("%s %s", where.getKey(), getSqlValue(where.getValue()));
+		}
+	}
 
-    public static List<Entity> getEntityList(String tableName, ResultSet rs) throws SQLException {
-        List<Entity> list = new ArrayList<>();
-        ResultSetMetaData rsMetaData = rs.getMetaData();
-        while (rs.next()) {
-            List<Field> fields = new ArrayList<>();
-            for (int i = 1; i <= rsMetaData.getColumnCount(); i++) {
-                fields.add(new Field(rsMetaData.getColumnLabel(i), rs.getObject(i)));
-            }
-            list.add(new Entity(tableName, fields));
-        }
-        return list;
-    }
+	private static String getSeparator(ComplexCondition.TypeEnum type) {
+		switch (type) {
+		case and:
+			return " and ";
+		case or:
+			return " or ";
+		default:
+			throw new RuntimeException("Unsupported Type");
+		}
+	}
 
-    public static int getCount(ResultSet rs) throws SQLException {
-        rs.next();
-        return rs.getObject("count", Integer.class);
-    }
+	private static String getSqlField(String field) {
+		return "`" + field + "`";
+	}
 
-    public static void closeQuietly(Closeable closeable) {
-        try {
-            if (closeable != null) {
-                closeable.close();
-            }
-        } catch (IOException e) {
-            // ignore
-        }
-    }
+	private static String getSqlValue(Object value) {
+		if (value instanceof String) {
+			return "'" + value.toString().replaceAll("'", "''") + "'";
+		}
+		if (value instanceof Integer) {
+			return value.toString();
+		}
+		if (value instanceof Long) {
+			return value.toString();
+		}
+		return value.toString();
+	}
 
-    public static void closeQuietly(AutoCloseable autoCloseable) {
-        try {
-            if (autoCloseable != null) {
-                autoCloseable.close();
-            }
-        } catch (Exception e) {
-            // ignore
-        }
-    }
+	public static String parseSorts(List<Sort> sorts) {
+		if (sorts == null || sorts.isEmpty()) {
+			return null;
+		}
+		List<String> list = new ArrayList<>();
+		for (Sort x : sorts) {
+			list.add(String.format("%s %s", getSqlField(x.getFieldName()), x.getType()));
+		}
+		return StringUtils.join(list, ",");
+	}
 
-    public static void rollbackQuietly(Connection connection) {
-        try {
-            if (connection != null) {
-                connection.rollback();
-            }
-        } catch (SQLException e) {
-            // ignore
-        }
-    }
+	public static String parse(PagerRequest pagerRequest) {
+		if (null == pagerRequest) {
+			return null;
+		}
+		return String.format("LIMIT %s OFFSET %s", pagerRequest.getPageSize(),
+				pagerRequest.getPageIndex() * pagerRequest.getPageSize());
+	}
 
-    public static String buildSql(SelectCommand command) {
-        StringBuffer sb = new StringBuffer("SELECT ");
-        sb.append(command.getFieldNames() == null || command.getFieldNames().isEmpty() ? "*" : StringUtils.join(command.getFieldNames(), ","));
-        sb.append(" FROM ").append(command.getTableName());
-        if (command.getLeftJoin() != null) {
-            sb.append(" LEFT JOIN ").append(command.getLeftJoin().getTableName()).append(" ON ").append(MysqlUtils.parse(command.getLeftJoin().getCondition()));
-        }
-        String where = MysqlUtils.parse(command.getWhere());
-        if (StringUtils.isNotBlank(where)) {
-            sb.append(" WHERE ").append(where);
-        }
-        String sort = MysqlUtils.parseSorts(command.getSorts());
-        if (StringUtils.isNotBlank(sort)) {
-            sb.append(" ORDER BY ").append(sort);
-        }
-        String pager = MysqlUtils.parse(command.getPager());
-        if (StringUtils.isNotBlank(pager)) {
-            sb.append(" ").append(pager);
-        }
-        return sb.toString();
-    }
-
-    public static String buildSql(DeleteCommand command) {
-        StringBuffer sb = new StringBuffer("DELETE FROM ").append(command.getTableName());
-        String where = MysqlUtils.parse(command.getWhere());
-        if (StringUtils.isNoneBlank(where)) {
-            sb.append(" WHERE ").append(where);
-        }
-        return sb.toString();
-    }
-
-    public static String buildSql(InsertCommand command) {
-        List<String> fieldNames = new ArrayList<>();
-        List<String> fieldValues = new ArrayList<>();
-        for (Field field : command.getEntity().getFields()) {
-            fieldNames.add(getSqlField(field.getName()));
-            String value = getSqlValue(field.getValue());
-            fieldValues.add(value == null ? "null" : value);
-        }
-        return String.format("INSERT INTO %s (%s) VALUES (%s)", command.getEntity().getTableName(), StringUtils.join(fieldNames, ","), StringUtils.join(fieldValues, ","));
-    }
-
-    public static String buildSql(UpdateCommand command) {
-        StringBuffer sb = new StringBuffer("UPDATE ").append(command.getTableName());
-        String fields = parseFields(command.getFields());
-        if (StringUtils.isNotBlank(fields)) {
-            sb.append(" SET ").append(fields);
-        }
-        String where = MysqlUtils.parse(command.getWhere());
-        if (StringUtils.isNotBlank(where)) {
-            sb.append(" WHERE ").append(where);
-        }
-        return sb.toString();
-    }
-
-    public static String parseFields(List<Field> fields) {
-        if (fields == null || fields.isEmpty()) {
-            return null;
-        }
-        List<String> list = new ArrayList<>();
-        for (Field x : fields) {
-            list.add(String.format("%s=%s", getSqlField(x.getName()), getSqlValue(x.getValue())));
-        }
-        return StringUtils.join(list, ",");
-    }
+	public static List<Entity> getEntityList(String tableName, ResultSet rs) throws SQLException {
+		List<Entity> list = new ArrayList<>();
+		ResultSetMetaData rsMetaData = rs.getMetaData();
+		while (rs.next()) {
+			List<Field> fields = new ArrayList<>();
+			for (int i = 1; i <= rsMetaData.getColumnCount(); i++) {
+				fields.add(new Field(rsMetaData.getColumnLabel(i), rs.getObject(i)));
+			}
+			list.add(new Entity(tableName, fields));
+		}
+		return list;
+	}
 }
